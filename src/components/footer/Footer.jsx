643ddcb82cf081a8adf4edbfe16a7850ae957167
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-03-30 17:35:03 +0500

    user project

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(){
+  return(
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All Right Reserved. <a href="https://www.youtube.com/@akrorweb">Akhror Solive</a></p>
+      </div>
+   </div>
+  )
+}
+
+export default Footer
