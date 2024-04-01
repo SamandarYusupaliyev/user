f6af6340cfbad05d5f255dd20d3a641e29181e20
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-01 07:40:52 +0500

    papaka

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
   return(
     <div className='footer'>
       <div className="footer-container container">
-        <p>All Right Reserved. <a href="https://www.youtube.com/@akrorweb">Akhror Solive</a></p>
+        <p>All Right Reserved. <a href="https://www.youtube.com/yusupaliyevsamandar">Yusupaliyev Samandar</a></p>
       </div>
    </div>
   )
